1db8f3cba98ea891979d8c0ce6d8d28334943261
ohfabe <fabiancorluka@gmail.com>
2025-08-22 11:45:56 -0400

    setup done

---
+++ backend/server/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const PORT = process.env.PORT || 3010
+
+const app = express();
+app.use(express.json());
+ 
+app.listen(PORT, () => {
+    console.log(PORT)
+})
+
+
