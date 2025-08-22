db11c9d76bfdfff8c92eab3c9b1e9d81edecd5f3
Chrishegyesi <bradshaw888@icloud.com>
2025-08-22 12:38:12 -0400

    testing to see why i dont hav epublic folder

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+    return (
+        <nav id = 'navbar'>
+            <div id = 'navbar-title-logo'>
+            <h2 id = 'title'>InnerBeautyRoom</h2>
+            <img src = '' id = 'navbar-logo' />
+            </div>
+            <div id = 'navbar-links'>
+                <link></link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
