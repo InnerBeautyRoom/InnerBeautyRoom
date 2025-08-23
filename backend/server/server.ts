80486d08e5d853b40d0c892cc35bb3a6c42c46f4
ohfabe <fabiancorluka@gmail.com>
2025-08-23 10:36:21 -0400

    booking calendar added still needs work

--- backend/server/server.ts
+++ backend/server/server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
+mongoose.connect(process.env.MONGO_URI!)
 const PORT = process.env.PORT || 3010
-
 const app = express();
 app.use(express.json());
+
+
  
 app.listen(PORT, () => {
     console.log(PORT)
