2771ba99f9866cb97385526e125297c49d7c26f5
ohfabe <fabiancorluka@gmail.com>
2025-08-25 12:01:42 -0400

    saving changes

--- frontend/components/Admin.tsx
+++ frontend/components/Admin.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
 const Admin = () => {
+    const [user, setUser] = useState('')
+    const [pass, setPass] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch('/api/admin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({user, pass})
+            })
+            if(!response.ok) {
+                throw new Error ('admin post response failed')
+            }
+            const data = await response.json()
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <div>
+        <div id = 'admin'>
             <h1>Admin</h1>
+            <form id = 'admin-form' onSubmit = {handleSubmit}>
+            <input type = 'text' placeholder = 'Username...' required value = {user} onChange = {(e) => setUser(e.target.value)}/>
+            <input type = 'password' placeholder = 'Password...' required value = {pass} onChange = {(e) => setPass(e.target.value)}/>
+            <button type = 'submit'>Submit</button>
+            </form>
         </div>
     )
 }
